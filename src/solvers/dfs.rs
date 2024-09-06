6d73d524c83bb97e5cbf47b378ae34f1dc519432
Nicolas M <mine.nicolas@gmail.com>
2024-09-06 21:23:22 +0200

    add timer and counter to evaluate hasher

--- src/solvers/dfs.rs
+++ src/solvers/dfs.rs
@@ -1,8 +1,10 @@
 use crate::board::Board;
+use fxhash::{FxHashSet, FxHasher64};
 use itertools;
 use std::{
     collections::{HashSet, VecDeque},
     hash::{DefaultHasher, Hash, Hasher},
+    time::Instant,
 };
 
 pub struct Solution {
@@ -21,17 +23,25 @@ impl Solution {
 
 pub fn dfs_solve(initial_state: &Board) -> String {
     let mut best_solution: Solution = Solution::new(initial_state.clone());
-    let mut visited: HashSet<u64> = HashSet::new();
+    let mut visited: HashSet<u64> = HashSet::default();
     let mut actions: VecDeque<String> = VecDeque::new();
+    let mut count = 0;
+    let timer = Instant::now();
 
     inner_dfs(
         &mut visited,
         &mut actions,
         &mut best_solution,
         initial_state,
+        &mut count,
     );
 
-    eprintln!("{}", best_solution.board.get_score());
+    eprintln!(
+        "Final best score: {} - {} plays in {:?}",
+        best_solution.board.get_score(),
+        count,
+        timer.elapsed()
+    );
     itertools::join(best_solution.actions, ";")
 }
 
@@ -40,14 +50,22 @@ fn inner_dfs(
     actions: &mut VecDeque<String>,
     best_solution: &mut Solution,
     initial_state: &Board,
+    count: &mut i32,
 ) {
     let all_regions = initial_state.compute_all_regions();
 
-    if initial_state.is_over() && initial_state.get_score() > best_solution.board.get_score() {
-        best_solution.board = initial_state.clone();
-        best_solution.actions = actions.clone();
+    if initial_state.is_over() {
+        if initial_state.get_score() > best_solution.board.get_score() {
+            best_solution.board = initial_state.clone();
+            best_solution.actions = actions.clone();
 
-        eprintln!("New best score: {}", best_solution.board.get_score());
+            eprintln!(
+                "New best score: {} - {} plays",
+                best_solution.board.get_score(),
+                count
+            );
+        }
+        *count += 1;
     }
 
     for region in all_regions {
@@ -55,7 +73,7 @@ fn inner_dfs(
 
         copy.play_region(&region);
 
-        let mut hasher = DefaultHasher::new();
+        let mut hasher = DefaultHasher::default();
         copy.hash(&mut hasher);
         let hash_value = hasher.finish();
         if visited.contains(&hash_value) {
@@ -67,7 +85,7 @@ fn inner_dfs(
         let (x, y) = Board::to_coordinates(idx);
         actions.push_back(format!("{} {}", x, y));
 
-        inner_dfs(visited, actions, best_solution, &copy);
+        inner_dfs(visited, actions, best_solution, &copy, count);
 
         actions.pop_back();
     }
