10c7fd7430cbdb442c62cfd4e53ac9525c79fbda
Nicolas M <mine.nicolas@gmail.com>
2024-09-08 15:50:01 +0200

    get the best score for the submission

---
+++ get_result.py
@@ -0,0 +1,17 @@
+import sqlite3
+import json
+
+best_results = {}
+score_per_hash = {}
+
+with sqlite3.connect("my_database.db") as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT hash, actions, score FROM my_objects WHERE name LIKE 'Standard%'")
+    rows = cursor.fetchall()
+    for hash, action, score in rows:
+        if score > best_results.get(hash, 0):
+            score_per_hash[hash] = score
+            best_results[hash] = action
+
+print(json.dumps(best_results, indent=4))
+print("total score: ", 2 * sum(score_per_hash.values()))
---
+++ run.bat
@@ -0,0 +1,27 @@
+@REM target\release\cg_samegame_rust.exe testcases/test1.json
+@REM target\release\cg_samegame_rust.exe testcases/test2.json
+@REM target\release\cg_samegame_rust.exe testcases/test3.json
+@REM target\release\cg_samegame_rust.exe testcases/test4.json
+@REM target\release\cg_samegame_rust.exe testcases/test5.json
+@REM target\release\cg_samegame_rust.exe testcases/test6.json
+target\release\cg_samegame_rust.exe testcases/test7.json
+target\release\cg_samegame_rust.exe testcases/test8.json
+@REM target\release\cg_samegame_rust.exe testcases/test9.json
+@REM target\release\cg_samegame_rust.exe testcases/test10.json
+@REM target\release\cg_samegame_rust.exe testcases/test12.json
+@REM target\release\cg_samegame_rust.exe testcases/test13.json
+@REM target\release\cg_samegame_rust.exe testcases/test14.json
+@REM target\release\cg_samegame_rust.exe testcases/test16.json
+@REM target\release\cg_samegame_rust.exe testcases/test17.json
+@REM target\release\cg_samegame_rust.exe testcases/test18.json
+@REM target\release\cg_samegame_rust.exe testcases/test19.json
+@REM target\release\cg_samegame_rust.exe testcases/test21.json
+@REM target\release\cg_samegame_rust.exe testcases/test22.json
+@REM target\release\cg_samegame_rust.exe testcases/test23.json
+@REM target\release\cg_samegame_rust.exe testcases/test24.json
+@REM target\release\cg_samegame_rust.exe testcases/test26.json
+@REM target\release\cg_samegame_rust.exe testcases/test27.json
+@REM target\release\cg_samegame_rust.exe testcases/test28.json
+@REM target\release\cg_samegame_rust.exe testcases/test29.json
+
+PAUSE
